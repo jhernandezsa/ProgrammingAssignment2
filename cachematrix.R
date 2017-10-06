f9294f5efa79784d2f88aa1225c6218c301852f7
jhernandezsa <jhernandezsa@gmail.com>
2017-10-06 06:44:40 -0700

    third commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,14 @@
+## two function 1. makeCacheMatrix() creates a special matrix, basically 
+## set the matrix (inverse) and get matrix (inverse)
+## 2. function is cachesolve() calculate inverse from matrix created by makeCacheMatrix(), 
+## first it verifies if inverse has been calculated, otherwise, it calculates inverse. 
 
 
+
+## objects X and minverse are initialized. these Objects store information. 
+## getters functions access information,
+## setters functions set information.
+## finally, a list is created, where each element is a function (getters or setters.)
 
 makeCacheMatrix <- function(x = matrix()) {
         minver <- NULL
@@ -16,7 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## Return a matrix that is the inverse of 'x'
-
+## function try to acess to a inverse passed in the argument (list)
+## the "if" checks if argument is NULL (is there any inverser?). conditional is TRUE check cache,
+## if conditional is false use solve function to calculate inverse
 
 
 cacheSolve <- function(x, ...) {
